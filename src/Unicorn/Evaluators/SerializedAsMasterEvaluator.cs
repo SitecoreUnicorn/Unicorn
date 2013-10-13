395b78d37dadeedcaa043bb1ae89c4a3f61bc5ad
Kam Figy <kamsar@gmail.com>
2013-10-12 23:49:32 -0700

    Refactor evaluator to be more extensible

--- src/Unicorn/Evaluators/SerializedAsMasterEvaluator.cs
+++ src/Unicorn/Evaluators/SerializedAsMasterEvaluator.cs
@@ -21,55 +21,35 @@ namespace Unicorn.Evaluators
 		{
 			Assert.ArgumentNotNull(serializedItem, "serializedItem");
 			Assert.ArgumentNotNull(progress, "progress");
-			
-			if(existingItem == null) return true;
+
+			if (existingItem == null) return true;
 
 			// see if the modified date is different in any version (because disk is master, ANY changes we want to force overwrite)
 			return serializedItem.Versions.Any(version =>
 				{
 					bool passedComparisons = false; // this flag lets us differentiate between items that we could not determine equality for, and items that just matched every criteria and don't need updating
 
-					
-					var serializedModified = version.Updated;
-					if (serializedModified != null)
+					var modifiedMatch = IsModifiedMatch(existingItem, serializedItem, version, progress);
+					if (modifiedMatch != null)
 					{
-						var itemModified = existingItem.GetLastModifiedDate(version.Language, version.VersionNumber);
-						var result = !serializedModified.Equals(itemModified);
-
-						if (result)
-						{
-							progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk modified {3}, Item modified {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedModified.Value.ToString("G"), itemModified.Value.ToString("G")), MessageType.Debug);
-
-							return true;
-						}
+						if (modifiedMatch.Value) return true;
 
 						passedComparisons = true;
 					}
 
 					// ocasionally a version will not have a modified date or a modified date will not get updated, only a revision change so we compare those as a backup
-					var serializedRevision = version.Revision;
-					
-					if (!string.IsNullOrEmpty(serializedRevision))
-					{
-						var itemRevision = existingItem.GetRevision(version.Language, version.VersionNumber);
-						var result = !serializedRevision.Equals(itemRevision);
+					var revisionMatch = IsRevisionMatch(existingItem, serializedItem, version, progress);
 
-						if (result)
-						{
-							progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk revision {3}, Item revision {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedRevision, itemRevision), MessageType.Debug);
-							return true;
-						}
+					if (revisionMatch != null)
+					{
+						if (revisionMatch.Value) return true;
 
 						passedComparisons = true;
 					}
 
 					// as a last check, see if the names do not match. Renames, for example, only change the name and not the revision or modified date (wtf?)
 					// unlike other checks, this one does not count as 'passing a comparison' if it fails to match a force. Having names be equal is not a strong enough measure of equality.
-					if (!serializedItem.Name.Equals(existingItem.Name))
-					{
-						progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk name {3}, Item name {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedItem.Name, existingItem.Name), MessageType.Debug);
-						return true;
-					}
+					if (IsNameMatch(existingItem, serializedItem, version, progress)) return true;
 
 					// if we get here and no comparisons were passed, we have no valid updated or revision to compare. Let's ignore the item as if it was a real item it'd have one of these.
 					if (!passedComparisons && !serializedItem.ItemPath.StartsWith("/sitecore/templates/System") && !serializedItem.ItemPath.StartsWith("/sitecore/templates/Sitecore Client")) // this occurs a lot in stock system templates - we ignore warnings for those as it's expected.
@@ -77,6 +57,49 @@ namespace Unicorn.Evaluators
 
 					return false;
 				});
+		}
+
+		protected virtual bool? IsModifiedMatch(ISourceItem existingItem, ISerializedItem serializedItem, SerializedVersion version, IProgressStatus progress)
+		{
+			var serializedModified = version.Updated;
+			if (serializedModified == null) return null;
+
+			var itemModified = existingItem.GetLastModifiedDate(version.Language, version.VersionNumber);
+
+			if (itemModified == null) return null;
+
+			var result = !serializedModified.Value.Equals(itemModified.Value);
+
+			if (result)
+				progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk modified {3}, Item modified {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedModified.Value.ToString("G"), itemModified.Value.ToString("G")), MessageType.Debug);
+
+			return result;
+		}
+
+		protected virtual bool? IsRevisionMatch(ISourceItem existingItem, ISerializedItem serializedItem, SerializedVersion version, IProgressStatus progress)
+		{
+			var serializedRevision = version.Revision;
+
+			if (string.IsNullOrEmpty(serializedRevision)) return null;
+
+			var itemRevision = existingItem.GetRevision(version.Language, version.VersionNumber);
+			var result = !serializedRevision.Equals(itemRevision);
+
+			if (result)
+				progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk revision {3}, Item revision {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedRevision, itemRevision), MessageType.Debug);
+
+			return result;
+		}
+
+		protected virtual bool IsNameMatch(ISourceItem existingItem, ISerializedItem serializedItem, SerializedVersion version, IProgressStatus progress)
+		{
+			if (!serializedItem.Name.Equals(existingItem.Name))
+			{
+				progress.ReportStatus(string.Format("{0} ({1} #{2}): Disk name {3}, Item name {4}", serializedItem.ItemPath, version.Language, version.VersionNumber, serializedItem.Name, existingItem.Name), MessageType.Debug);
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
