35cf531b741919f805347efb58ffa3d37077d60b
Kam Figy <kamsar@gmail.com>
2013-11-26 20:40:45 -0800

    Rewrite conflict processor to work with data providers as much as possible, and ignore items that Unicorn does not manage

--- src/Unicorn/SerializationConflictProcessor.cs
+++ src/Unicorn/SerializationConflictProcessor.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
 using Sitecore;
 using Sitecore.Diagnostics;
 using Sitecore.Pipelines.Save;
-using Sitecore.Data.Serialization.ObjectModel;
 using Sitecore.Data.Items;
-using Sitecore.Data.Serialization;
 using Sitecore.Data.Fields;
-using System.Threading;
+using Unicorn.Data;
+using Unicorn.Predicates;
+using Unicorn.Serialization;
+using Unicorn.Serialization.Sitecore;
 
 namespace Unicorn
 {
@@ -24,8 +23,30 @@ namespace Unicorn
 	/// This handler verifies that the pre-save state of the item matches the field values present in the serialized item on disk (if it's present).
 	/// If they dont match a sheer warning is shown.
 	/// </summary>
+	/// <remarks>
+	/// Note that this solution does NOT save you from every possible conflict situation. Some saves do not run the saveUI pipeline, for example:
+	/// - Renaming any item
+	/// - Moving items
+	/// - Changing template field values (source, type) in the template builder
+	/// 
+	/// This handler is here to attempt to keep you from shooting yourself in the foot, but you really need to sync Unicorn after every update pulled from SCM.
+	/// </remarks>
 	public class SerializationConflictProcessor
 	{
+		private readonly ISerializationProvider _serializationProvider;
+		private readonly IPredicate _predicate;
+
+		public SerializationConflictProcessor() : this(new SitecoreSerializationProvider(), new SerializationPresetPredicate(new SitecoreSourceDataProvider()))
+		{
+			
+		}
+
+		public SerializationConflictProcessor(ISerializationProvider serializationProvider, IPredicate predicate)
+		{
+			_serializationProvider = serializationProvider;
+			_predicate = predicate;
+		}
+
 		public void Process(SaveArgs args)
 		{
 			Assert.ArgumentNotNull(args, "args");
@@ -46,19 +67,7 @@ namespace Unicorn
 			// no errors detected, we're good
 			if (string.IsNullOrEmpty(error)) return;
 
-			// occasionally if you serialize an item "too fast" after changing it, the ShadowWriter's async operations
-			// will be in process or have not yet begun to update the serialized item.
-			// in these circumstances, we wish to treat a "conflict" or exception as a possible false positive, wait a bit for the ShadowWriter,
-			// and try again. If it fails 4x in a row, 500ms apart, it's considered an unrecoverable conflict and we show the error.
-			const int retries = 4;
-			for (int retryCount = 0; retryCount < retries; retryCount++)
-			{
-				Thread.Sleep(500);
-				error = GetErrorValue(args);
-				if (string.IsNullOrEmpty(error)) return;
-			}
-
-			Sitecore.Web.UI.Sheer.SheerResponse.Confirm(error);		
+			Sitecore.Web.UI.Sheer.SheerResponse.Confirm(error);
 			args.WaitForPostBack();
 		}
 
@@ -70,13 +79,25 @@ namespace Unicorn
 			{
 				foreach (var item in args.Items)
 				{
-					Item existingItem = Client.ContentDatabase.GetItem(item.ID);
-					SyncItem serializedVersion = ReadSerializedVersion(existingItem);
+					Item existingItem = Client.ContentDatabase.GetItem(item.ID, item.Language, item.Version);
+
+					Assert.IsNotNull(existingItem, "Existing item {0} did not exist! This should never occur.", item.ID);
+
+					var existingSitecoreItem = new SitecoreSourceItem(existingItem);
+
+					// ignore conflicts on items that Unicorn is not managing
+					if (!_predicate.Includes(existingSitecoreItem).IsIncluded) continue;
+
+					ISerializedReference serializedReference = _serializationProvider.GetReference(existingSitecoreItem);
+					
+					if(serializedReference == null) continue;
 
 					// not having an existing serialized version means no possibility of conflict here
-					if (serializedVersion == null) continue;
+					ISerializedItem serializedItem = _serializationProvider.GetItem(serializedReference);
 
-					var fieldIssues = GetFieldSyncStatus(existingItem, serializedVersion);
+					if (serializedItem == null) continue;
+
+					var fieldIssues = GetFieldSyncStatus(existingSitecoreItem, serializedItem);
 
 					if (fieldIssues.Count == 0) continue;
 
@@ -87,14 +108,18 @@ namespace Unicorn
 				if (results.Count == 0) return null;
 
 				var sb = new StringBuilder();
-				sb.Append("CRITICAL:\n");
-				sb.Append("Item state conflicted with existing serialized value. Chances are you need to sync your database with the filesystem. The following fields had problems:\n");
+				sb.Append("CRITICAL MESSAGE FROM UNICORN:\n");
+				sb.Append("You need to run a Unicorn sync. The following fields did not match the serialized version:\n");
+
 				foreach (var item in results)
 				{
-					sb.AppendFormat("\n{0}: {1}", item.Key.DisplayName, string.Join(", ", item.Value.Select(x => x.FieldName)));
+					if(results.Count > 1)
+						sb.AppendFormat("\n{0}: {1}", item.Key.DisplayName, string.Join(", ", item.Value.Select(x => x.FieldName)));
+					else
+						sb.AppendFormat("\n{0}", string.Join(", ", item.Value.Select(x => x.FieldName)));
 				}
 
-				sb.Append("\n\nDo you want to overwrite anyway? You may cause someone else to lose committed work by doing this.");
+				sb.Append("\n\nDo you want to overwrite anyway?\nTHIS MAY CAUSE LOST WORK.");
 
 				return sb.ToString();
 			}
@@ -105,11 +130,11 @@ namespace Unicorn
 			}
 		}
 
-		private IList<FieldDesynchronization> GetFieldSyncStatus(Item item, SyncItem serializedItem)
+		private IList<FieldDesynchronization> GetFieldSyncStatus(SitecoreSourceItem item, ISerializedItem serializedItem)
 		{
 			var desyncs = new List<FieldDesynchronization>();
 
-			var serializedVersion = serializedItem.Versions.FirstOrDefault(x => x.Version == item.Version.Number.ToString(CultureInfo.InvariantCulture) && x.Language == item.Language.Name);
+			var serializedVersion = serializedItem.Versions.FirstOrDefault(x => x.VersionNumber == item.InnerItem.Version.Number && x.Language == item.InnerItem.Language.Name);
 			
 			if (serializedVersion == null)
 			{
@@ -117,43 +142,31 @@ namespace Unicorn
 				return desyncs;
 			}
 
-			item.Fields.ReadAll();
+			item.InnerItem.Fields.ReadAll();
 
-			foreach (Field field in item.Fields)
+			foreach (Field field in item.InnerItem.Fields)
 			{
-				if (field.ID == FieldIDs.Revision || field.ID == FieldIDs.Updated) continue; // we're doing a data comparison here - revision and updated don't matter
+				if (field.ID == FieldIDs.Revision || field.ID == FieldIDs.Updated || field.ID == FieldIDs.Created || field.ID == FieldIDs.CreatedBy || field.ID == FieldIDs.UpdatedBy) continue; 
+				// we're doing a data comparison here - revision, created (by), updated (by) don't matter
 				// skipping these fields allows us to ignore spurious saves the template builder makes to unchanged items being conflicts
 			
 				// find the field in the serialized item in either versioned or shared fields
-				var serializedField = serializedVersion.Fields.FirstOrDefault(x => x.FieldID == field.ID.ToString()) ??
-									serializedItem.SharedFields.FirstOrDefault(x => x.FieldID == field.ID.ToString());
+				string serializedField;
+				
+				if(!serializedVersion.Fields.TryGetValue(field.ID.ToString(), out serializedField))
+					serializedItem.SharedFields.TryGetValue(field.ID.ToString(), out serializedField);
 
 				// we ignore if the field doesn't exist in the serialized item. This is because if you added a field to a template,
 				// that does not immediately re-serialize all items based on that template so it's likely innocuous - we're not overwriting anything.
 				if (serializedField == null) continue;
 
-				if (!serializedField.FieldValue.Equals(field.Value, StringComparison.Ordinal))
+				if (!serializedField.Equals(field.Value, StringComparison.Ordinal))
 				{
-					desyncs.Add(new FieldDesynchronization(serializedField.FieldName));
+					desyncs.Add(new FieldDesynchronization(field.Name));
 				}
 			}
 
 			return desyncs;
-		}
-
-		private SyncItem ReadSerializedVersion(Item item)
-		{
-			Assert.IsNotNull(item, "Item cannot be null");
-
-			var reference = new ItemReference(item);
-			var path = PathUtils.GetFilePath(reference.ToString());
-
-			if (!File.Exists(path)) return null;
-
-			using (var file = new StreamReader(path))
-			{
-				 return SyncItem.ReadItem(new Tokenizer(file));
-			}
 		}
 
 		private class FieldDesynchronization
