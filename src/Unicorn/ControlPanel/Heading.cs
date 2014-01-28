a6ea210c98d68573f20f754b5d200c3edb10625d
Kam Figy <kamsar@gmail.com>
2014-01-28 08:46:57 -0800

    Add warnings to control panel if predicate does not resolve to any valid roots

--- src/Unicorn/ControlPanel/Heading.cs
+++ src/Unicorn/ControlPanel/Heading.cs
@@ -1,11 +1,19 @@
 ﻿using System.Linq;
 using System.Reflection;
 using System.Web.UI;
+using Unicorn.Predicates;
 
 namespace Unicorn.ControlPanel
 {
 	public class Heading : IControlPanelControl
 	{
+		private readonly IPredicate _predicate;
+
+		public Heading(IPredicate predicate)
+		{
+			_predicate = predicate;
+		}
+
 		public bool HasSerializedItems { get; set; }
 		public bool IsAuthenticated { get; set; }
 
@@ -20,7 +28,10 @@ namespace Unicorn.ControlPanel
 
 				if (!HasSerializedItems)
 				{
-					writer.Write("<p class=\"warning\">Warning: you have not serialized any items yet. Unicorn cannot operate until this is complete. Please review the configuration below and then perform initial serialization if it is accurate. If you need to change your config, see App_Config\\Include\\Serialization.config.</p>");
+					if(_predicate.GetRootItems().Length > 0)
+						writer.Write("<p class=\"warning\">Warning: you have not serialized any items yet. Unicorn cannot operate until this is complete. Please review the configuration below and then perform initial serialization if it is accurate. If you need to change your config, see App_Config\\Include\\Serialization.config.</p>");
+					else 
+						writer.Write("<p class=\"warning\">Warning: your current predicate configuration does not have any valid root items defined. Nothing will be serialized until valid root items to start serializing from can be resolved. Please review your predicate configuration.</p>");
 				}
 			}
 		}
--- src/Unicorn/ControlPanel/InitialSetup.cs
+++ src/Unicorn/ControlPanel/InitialSetup.cs
@@ -1,15 +1,31 @@
 ﻿using System.Web.UI;
+using Unicorn.Predicates;
 
 namespace Unicorn.ControlPanel
 {
 	public class InitialSetup : IControlPanelControl
 	{
+		private readonly IPredicate _predicate;
+
+		public InitialSetup(IPredicate predicate)
+		{
+			_predicate = predicate;
+		}
+
 		public void Render(HtmlTextWriter writer)
 		{
 			writer.Write("<h2>Initial Setup</h2>");
-			writer.Write("<p>Would you like to perform an initial serialization of all configured items using the options outlined above now? This is required to start using Unicorn.</p>");
 
-			writer.Write("<a class=\"button\" href=\"?verb=Reserialize\">Perform Initial Serialization</a>");
+			if (_predicate.GetRootItems().Length > 0)
+			{
+				writer.Write("<p>Would you like to perform an initial serialization of all configured items using the options outlined above now? This is required to start using Unicorn.</p>");
+
+				writer.Write("<a class=\"button\" href=\"?verb=Reserialize\">Perform Initial Serialization</a>");
+			}
+			else
+			{
+				writer.Write("<p>Cannot perform initial serialization until the predicate configuration includes valid root items.</p>");
+			}
 		}
 	}
 }
