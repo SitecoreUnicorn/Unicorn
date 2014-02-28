f04996ee5b3c1d08b5d74ff41299d1710150f5bd
Kam Figy <kamsar@gmail.com>
2014-02-27 20:17:52 -0800

    Switch to "new-style" NuGet package restore (http://blog.davidebbo.com/2014/01/the-right-way-to-restore-nuget-packages.html)

--- Build/Build NuGet.cmd
+++ Build/Build NuGet.cmd
@@ -1,2 +1,5 @@
-..\.nuget\nuget.exe pack ..\src\Unicorn\Unicorn.csproj -Build -Symbols -Properties Configuration=Release
+
+@echo off
+"%~dp0..\tools\nuget.exe" restore "%~dp0..\Unicorn.sln"
+"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Unicorn\Unicorn.csproj" -Build -Symbols -Properties Configuration=Release
 PAUSE
