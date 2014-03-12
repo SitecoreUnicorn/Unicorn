cc0bcb86321c4295183c32de85985374bef03960
Kam Figy <kamsar@gmail.com>
2014-03-12 15:57:42 -0700

    Normalized formatting, tested

--- Source/Unicorn/FilteredItemLoader.cs
+++ Source/Unicorn/FilteredItemLoader.cs
@@ -10,78 +10,78 @@ using Sitecore.SecurityModel;
 
 namespace Unicorn
 {
-    public class FilteredItemLoader
-    {
-        public void Process(PipelineArgs args)
-        {
-            var syncDatabase = Settings.GetBoolSetting("UnicornSyncDatabase", false);
-            if (!syncDatabase)
-            {
-                Log.Warn("Unicorn automatic sync to database is disabled.", this);
-                return;
-            }
+	public class FilteredItemLoader
+	{
+		public void Process(PipelineArgs args)
+		{
+			var syncDatabase = Settings.GetBoolSetting("UnicornSyncDatabase", false);
+			if (!syncDatabase)
+			{
+				Log.Warn("Unicorn automatic sync to database is disabled.", this);
+				return;
+			}
 
-            // load the requested (or default) preset
-            var presets = SerializationUtility.GetPreset("default").ToList();
-            if (!presets.Any())
-            {
-                Log.Warn("Unicorn preset did not exist in configuration.", this);
-                return;
-            }
+			// load the requested (or default) preset
+			var presets = SerializationUtility.GetPreset("default").ToList();
+			if (!presets.Any())
+			{
+				Log.Warn("Unicorn preset did not exist in configuration.", this);
+				return;
+			}
 
-            presets.ForEach(ProcessPreset);
+			presets.ForEach(ProcessPreset);
 
-            var publishDatabase = Settings.GetBoolSetting("UnicornPublishDatabase", false);
-            if (!publishDatabase)
-            {
-                return;
-            }
+			var publishDatabase = Settings.GetBoolSetting("UnicornPublishDatabase", false);
+			if (!publishDatabase)
+			{
+				return;
+			}
 
-            presets.ForEach(PublishPreset);
-        }
+			presets.ForEach(PublishPreset);
+		}
 
-        private void ProcessPreset(IncludeEntry preset)
-        {
-            var options = new AdvancedLoadOptions(preset)
-            {
-                ForceUpdate = false,
-                DeleteOrphans = true
-            };
+		private void ProcessPreset(IncludeEntry preset)
+		{
+			var options = new AdvancedLoadOptions(preset)
+			{
+				ForceUpdate = false,
+				DeleteOrphans = true
+			};
 
-            try
-            {
-                using (new SecurityDisabler())
-                {
-                    new SerializationLoader().LoadTree(options);
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Unicorn unable to process preset", ex, this);
-                throw;
-            }
-        }
+			try
+			{
+				using (new SecurityDisabler())
+				{
+					new SerializationLoader().LoadTree(options);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Error("Unicorn unable to process preset", ex, this);
+				throw;
+			}
+		}
 
-        private void PublishPreset(IncludeEntry preset)
-        {
-            if (preset.Database != "master")
-            {
-                return;
-            }
+		private void PublishPreset(IncludeEntry preset)
+		{
+			if (preset.Database != "master")
+			{
+				return;
+			}
 
-            var master = Factory.GetDatabase("master");
-            var target = Factory.GetDatabase("web");
-            var home = master.GetItem(preset.Path);
-            Database[] targetDatabases = { target };
-            try
-            {
-                PublishManager.PublishItem(home, targetDatabases, master.Languages, true, true);
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Unicorn unable to publish preset", ex, this);
-                throw;
-            }
-        }
-    }
+			var master = Factory.GetDatabase("master");
+			var target = Factory.GetDatabase("web");
+			var home = master.GetItem(preset.Path);
+			Database[] targetDatabases = { target };
+			try
+			{
+				PublishManager.PublishItem(home, targetDatabases, master.Languages, true, true);
+			}
+			catch (Exception ex)
+			{
+				Log.Error("Unicorn unable to publish preset", ex, this);
+				throw;
+			}
+		}
+	}
 }
