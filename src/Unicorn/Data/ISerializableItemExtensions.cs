7213035d69c6da2aee4e71ec98efe25cd7229b3f
Kam Figy <kamsar@gmail.com>
2015-07-09 18:31:13 -0700

    refactoring

--- src/Unicorn/Data/ISerializableItemExtensions.cs
+++ src/Unicorn/Data/ISerializableItemExtensions.cs
@@ -6,15 +6,6 @@ namespace Unicorn.Data
 {
 	public static class SerializableItemExtensions
 	{
-		/// <summary>
-		/// Helper method to get a specific version from a source item, if it exists
-		/// </summary>
-		/// <returns>Null if the version does not exist or the version if it exists</returns>
-		public static ISerializableVersion GetVersion(this ISerializableItem sourceItem, string language, int versionNumber)
-		{
-			return sourceItem.Versions.FirstOrDefault(x => x.Language.Name.Equals(language, StringComparison.OrdinalIgnoreCase) && x.VersionNumber == versionNumber);
-		}
-
 		public static string GetDisplayIdentifier(this ISerializableItem item)
 		{
 			return string.Format("{0}:{1} ({2})", item.DatabaseName, item.Path, item.Id);
