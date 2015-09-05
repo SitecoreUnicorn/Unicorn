715d36409d391160f3de72ea1ad76dd41f10ae30
Kam Figy <kamsar@gmail.com>
2015-09-05 00:15:35 -0700

    Raise serialization complete async, as it can take like 15 seconds

--- src/Unicorn/Pipelines/UnicornSyncEnd/SendSerializationCompleteEvent.cs
+++ src/Unicorn/Pipelines/UnicornSyncEnd/SendSerializationCompleteEvent.cs
@@ -4,6 +4,7 @@ using Sitecore.Data;
 using Sitecore.Data.Serialization;
 using Sitecore.Diagnostics;
 using Sitecore.Eventing;
+using Sitecore.Jobs;
 using Unicorn.Predicates;
 
 namespace Unicorn.Pipelines.UnicornSyncEnd
@@ -12,7 +13,7 @@ namespace Unicorn.Pipelines.UnicornSyncEnd
 	{
 		public void Process(UnicornSyncEndPipelineArgs args)
 		{
-			var databases =	args.SyncedConfigurations.SelectMany(config => config.Resolve<IPredicate>().GetRootPaths())
+			var databases = args.SyncedConfigurations.SelectMany(config => config.Resolve<IPredicate>().GetRootPaths())
 					.Select(path => path.DatabaseName)
 					.Distinct();
 
@@ -26,6 +27,13 @@ namespace Unicorn.Pipelines.UnicornSyncEnd
 		{
 			Assert.ArgumentNotNullOrEmpty(databaseName, "databaseName");
 
+			// raising this event can take a long time. like 16 seconds. So we boot it as a job so it can go in the background.
+			Job asyncSerializationFinished = new Job(new JobOptions("Raise deserialization complete async", "serialization", "shell", this, "RaiseEvent", new[] { databaseName }));
+			JobManager.Start(asyncSerializationFinished);
+		}
+
+		public virtual void RaiseEvent(string databaseName)
+		{
 			EventManager.RaiseEvent(new SerializationFinishedEvent());
 			Database database = Factory.GetDatabase(databaseName, false);
 			if (database != null)
