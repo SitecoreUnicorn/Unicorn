337ca6faf5948f1666a912b284e6f8692aeb107b
Kam Figy <kamsar@gmail.com>
2015-09-14 13:16:14 -0700

    3.0b8

--- src/Unicorn/Properties/AssemblyInfo.cs
+++ src/Unicorn/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta07")]
+[assembly: AssemblyInformationalVersion("3.0.0-beta08")]
 
 [assembly: InternalsVisibleTo("Unicorn.Tests")]
