6531e729f3a2719041f2da932d78e6dca3e73c4f
Kam Figy <kamsar@gmail.com>
2015-09-15 07:34:12 -0700

    Bork bork

--- src/Unicorn/Predicates/SerializationPresetPredicate.cs
+++ src/Unicorn/Predicates/SerializationPresetPredicate.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Xml;
 using Rainbow.Model;
-using Rainbow.Predicates;
 using Rainbow.Storage;
 using Sitecore.Data.Serialization.Presets;
 using Sitecore.Diagnostics;
