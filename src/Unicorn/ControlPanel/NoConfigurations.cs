590a682128c17f3d4c1edf0eee46840cbcefc670
Kam Figy <kamsar@gmail.com>
2015-09-24 08:08:27 -0700

    Make language a bit less condescending in places
    http://bradfrost.com/blog/post/just/ dude is right

--- src/Unicorn/ControlPanel/NoConfigurations.cs
+++ src/Unicorn/ControlPanel/NoConfigurations.cs
@@ -14,7 +14,7 @@ namespace Unicorn.ControlPanel
 
 			writer.Write("<p>No Unicorn configurations have been defined. A configuration is a set of items to have Unicorn sync; in advanced usage can also customize how Unicorn works for that configuration.</p>");
 			writer.Write("<p>To help with defining your first configuration, see the <code>Unicorn.Configs.Default.example</code> that should be in App_Config\\Include\\Unicorn for an example you can use as a starting point.</p>");
-			writer.Write("<p>Just duplicate the example file and change its extension to .config, then read through the comments in the file to assist you in setting up. When you're done just refresh this page and you can perform your initial serialization.</p>");
+			writer.Write("<p>Duplicate the example file and change its extension to .config, then read through the comments in the file to assist you in setting up. When you're done, refresh this page and you can perform your initial serialization.</p>");
 			writer.Write("</article>");
 		}
 	}
