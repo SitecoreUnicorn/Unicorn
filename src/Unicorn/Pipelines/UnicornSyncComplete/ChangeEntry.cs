4845b1272fc3d4b177e88588e6eded5d8f14f9c0
Kam Figy <kamsar@gmail.com>
2015-11-20 10:19:57 -0800

    Clear dictionary caches after syncing dictionary entries
    Ref #83

--- src/Unicorn/Pipelines/UnicornSyncComplete/ChangeEntry.cs
+++ src/Unicorn/Pipelines/UnicornSyncComplete/ChangeEntry.cs
@@ -11,12 +11,14 @@ namespace Unicorn.Pipelines.UnicornSyncComplete
 			{
 				Id = itemData.Id;
 				DatabaseName = itemData.DatabaseName;
+				TemplateId = itemData.TemplateId;
 			}
 			
 			ChangeType = type;
 		}
 
 		public Guid? Id { get; private set; }
+		public Guid? TemplateId { get; private set; }
 		public string DatabaseName { get; private set; }
 		public ChangeType ChangeType { get; private set; }
 	}
