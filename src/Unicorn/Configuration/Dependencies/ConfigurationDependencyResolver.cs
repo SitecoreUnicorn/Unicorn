86f7fe7ef7c12c446bcb0384bb44e5321c2a6371
Kam Figy <kamsar@gmail.com>
2015-12-14 23:25:16 -0800

    Remove implicit dependencies due to excessive coupling
    There was way too much coupling and service locator going on here to write effective tests.
    This feature is being removed unless it can be reintroduced with less coupling and with prewritten tests.

--- src/Unicorn/Configuration/Dependencies/ConfigurationDependencyResolver.cs
+++ src/Unicorn/Configuration/Dependencies/ConfigurationDependencyResolver.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unicorn.Data;
-using Unicorn.Predicates;
 
 namespace Unicorn.Configuration.Dependencies
 {
@@ -47,8 +45,7 @@ namespace Unicorn.Configuration.Dependencies
 			if (DependencyCache.ContainsKey(_configuration))
 				return DependencyCache[_configuration];
 
-			var explicitDependencies = GetExplicitDependencies(_configuration);
-			var dependencies = GetImplicitDependencies(_configuration).Union(explicitDependencies).ToArray();
+			var dependencies = GetDependencies(_configuration);
 
 			DependencyCache.Add(_configuration, dependencies);
 
@@ -58,70 +55,15 @@ namespace Unicorn.Configuration.Dependencies
 		/// <summary>
 		/// Finds configurations that explicitly depend on the current configuration (e.g. declare a dependency)
 		/// </summary>
-		protected virtual IEnumerable<IConfigurationDependency> GetExplicitDependencies(IConfiguration configuration)
+		protected virtual IConfigurationDependency[] GetDependencies(IConfiguration configuration)
 		{
 			if (configuration.Dependencies == null)
 				return new IConfigurationDependency[0];
 
 			return AllConfigurations
 				.Where(config => configuration.Dependencies.Any(dependency => dependency.Equals(config.Name, StringComparison.OrdinalIgnoreCase)))
-				.Select(config => new ExplicitConfigurationDependency(config));
-		}
-
-		/// <summary>
-		/// Finds configurations that implicitly depend on this one based on paths. 
-		/// </summary>
-		/// <remarks>
-		/// Implicit dependency requires some explanation. This is a situation such as:
-		/// - Config A includes /sitecore/content/Foo, without children
-		/// - Config B includes /sitecore/content/Foo/Lol
-		/// If and only if the /sitecore/content/Foo item does not yet exist Config B will gain an implicit dependency on Config A,
-		/// such that Config B does not fail to load due to the missing parent contained in Config A.
-		/// </remarks>
-		protected virtual IEnumerable<IConfigurationDependency> GetImplicitDependencies(IConfiguration configuration)
-		{
-			var otherConfigurations = AllConfigurations
-				.Where(config => !config.Name.Equals(configuration.Name, StringComparison.OrdinalIgnoreCase))
+				.Select(config => (IConfigurationDependency)new ExplicitConfigurationDependency(config))
 				.ToArray();
-
-			var currentPredicate = configuration.Resolve<PredicateRootPathResolver>();
-			var sourceDataStore = configuration.Resolve<ISourceDataStore>();
-			var rootPaths = currentPredicate.GetRootPaths();
-
-			foreach (var rootPath in rootPaths)
-			{
-				var parentPath = GetParentPath(rootPath.Path);
-
-				// if the include was of /sitecore parent has no meaning, ignore it
-				if (string.IsNullOrEmpty(parentPath)) continue;
-
-				// If the parent is found in the source data store, then we do not need to check for implicit dependencies
-				// (e.g. this config's load will not fail due to a missing parent item)
-				var item = sourceDataStore.GetByPath(parentPath, rootPath.DatabaseName).FirstOrDefault();
-
-				if (item != null) continue;
-
-				// Look for parent in other configurations, if any exist
-				foreach (var otherConfiguration in otherConfigurations)
-				{
-					// If the parent is found in a target data store, then we have a dependency
-					var targetDataStore = otherConfiguration.Resolve<ITargetDataStore>();
-
-					item = targetDataStore.GetByPath(parentPath, rootPath.DatabaseName).FirstOrDefault();
-
-					if (item == null) continue;
-
-					yield return new ImplicitConfigurationDependency(otherConfiguration, rootPath, parentPath);
-
-					break;
-				}
-			}
-		}
-
-		protected static string GetParentPath(string rootPath)
-		{
-			var path = rootPath.TrimEnd('/');
-			return path.Substring(0, path.LastIndexOf('/'));
 		}
 	}
 }
