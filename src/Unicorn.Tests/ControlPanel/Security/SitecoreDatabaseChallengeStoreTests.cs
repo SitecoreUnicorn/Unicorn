cdcd076658b1db6f6b4a487a5505080301793c76
Kam Figy <kamsar@gmail.com>
2015-12-17 11:19:26 -0800

    Working challenge store tests

--- src/Unicorn.Tests/ControlPanel/Security/SitecoreDatabaseChallengeStoreTests.cs
+++ src/Unicorn.Tests/ControlPanel/Security/SitecoreDatabaseChallengeStoreTests.cs
@@ -1,72 +1,73 @@
-﻿// Unfortunately FakeDb doesn't understand it when you're creating templates dynamically
-// so it doesn't know how to test this challenge store
+﻿using System.Threading;
+using FluentAssertions;
+using MicroCHAP.Server;
+using Sitecore.FakeDb;
+using Unicorn.ControlPanel.Security;
+using Xunit;
 
-//using System.Threading;
-//using FluentAssertions;
-//using MicroCHAP.Server;
-//using Sitecore.Data;
-//using Unicorn.ControlPanel.Security;
-//using Xunit;
+namespace Unicorn.Tests.ControlPanel.Security
+{
+	public class SitecoreDatabaseChallengeStoreTests
+	{
+		[Theory, AutoDbData]
+		public void ConsumeChallenge_ShouldReturnFalseIfChallengeDoesNotExist(Db db)
+		{
+			var store = CreateTestStore(db);
 
-//namespace Unicorn.Tests.ControlPanel.Security
-//{
-//	public class InMemoryChallengeStoreTests
-//	{
-//		[Theory, AutoDbData]
-//		public void ConsumeChallenge_ShouldReturnFalseIfChallengeDoesNotExist(Database db)
-//		{
-//			var store = CreateTestStore(db);
+			store.ConsumeChallenge("FAKE").Should().BeFalse();
+		}
 
-//			store.ConsumeChallenge("FAKE").Should().BeFalse();
-//		}
+		[Theory, AutoDbData]
+		public void ConsumeChallenge_ShouldReturnFalseIfChallengeIsTooOld(Db db)
+		{
+			var store = CreateTestStore(db);
 
-//		[Theory, AutoDbData]
-//		public void ConsumeChallenge_ShouldReturnFalseIfChallengeIsTooOld(Database db)
-//		{
-//			var store = CreateTestStore(db);
+			store.AddChallenge("FAKE", 500);
 
-//			store.AddChallenge("FAKE", 100);
+			Thread.Sleep(550);
 
-//			Thread.Sleep(150);
+			store.ConsumeChallenge("FAKE").Should().BeFalse();
+		}
 
-//			store.ConsumeChallenge("FAKE").Should().BeFalse();
-//		}
+		[Theory, AutoDbData]
+		public void ConsumeChallenge_ShouldReturnTrue_IfTokenIsValid(Db db)
+		{
+			var store = CreateTestStore(db);
 
-//		[Theory, AutoDbData]
-//		public void ConsumeChallenge_ShouldReturnTrue_IfTokenIsValid(Database db)
-//		{
-//			var store = CreateTestStore(db);
+			store.AddChallenge("FAKE", 1000);
 
-//			store.AddChallenge("FAKE", 100);
+			store.ConsumeChallenge("FAKE").Should().BeTrue();
+		}
 
-//			store.ConsumeChallenge("FAKE").Should().BeTrue();
-//		}
+		[Theory, AutoDbData]
+		public void ConsumeChallenge_ShouldNotAllowReusingTokens(Db db)
+		{
+			var store = CreateTestStore(db);
 
-//		[Theory, AutoDbData]
-//		public void ConsumeChallenge_ShouldNotAllowReusingTokens(Database db)
-//		{
-//			var store = CreateTestStore(db);
+			store.AddChallenge("FAKE", 1000);
 
-//			store.AddChallenge("FAKE", 100);
+			store.ConsumeChallenge("FAKE").Should().BeTrue();
+			store.ConsumeChallenge("FAKE").Should().BeFalse();
+		}
 
-//			store.ConsumeChallenge("FAKE").Should().BeTrue();
-//			store.ConsumeChallenge("FAKE").Should().BeFalse();
-//		}
+		private IChallengeStore CreateTestStore(Db db)
+		{
+			// fakedb doesn't like it when we create our own template with the item API. So we'll prep that in advance.
+			db.Add(new DbTemplate("Authentication Challenge")
+			{
+				new DbField("Expires")
+			});
 
-//		private IChallengeStore CreateTestStore(Database db)
-//		{
+			return new TestSitecoreDatabaseChallengeStore();
+		}
 
-//			return new TestSitecoreDatabaseChallengeStore(db.n);
-//		}
+		private class TestSitecoreDatabaseChallengeStore : SitecoreDatabaseChallengeStore
+		{
+			public TestSitecoreDatabaseChallengeStore() : base("master")
+			{
+			}
 
-//		private class TestSitecoreDatabaseChallengeStore : SitecoreDatabaseChallengeStore
-//		{
-//			public TestSitecoreDatabaseChallengeStore(string databaseName) : base(databaseName)
-//			{
-//			}
-
-//			// path that exists in FakeDb by default
-//			protected override string TemplateParent => "/sitecore/templates";
-//		}
-//	}
-//}
+			protected override string TemplateParent { get { return "/sitecore/templates"; } }
+		}
+	}
+}
