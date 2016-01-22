b66f3134e1e47117ccf5b274d6ca0b0d6e1c3ca5
Kam Figy <kamsar@gmail.com>
2016-01-21 16:26:32 -0800

    3.1b12

--- src/Unicorn/Properties/AssemblyInfo.cs
+++ src/Unicorn/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Unicorn")]
 [assembly: AssemblyDescription("Sitecore serialization helper framework")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.1.0.0")]
 [assembly: AssemblyFileVersion("3.1.0.0")]
-[assembly: AssemblyInformationalVersion("3.1.0-beta11")]
+[assembly: AssemblyInformationalVersion("3.1.0-beta12")]
 
 [assembly: InternalsVisibleTo("Unicorn.Tests")]
