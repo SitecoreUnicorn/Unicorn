7f86b28023f68758407ef75367ae67be4d4568db
Andrii Snihyr <war_chief@bigmir.net>
2016-01-25 22:18:00 +0200

    Pipeline for checking configuration health/status

--- src/Unicorn/ControlPanel/VisualStudio/Pipelines/UnicornControlPanelRequest/ConfigurationsVerb.cs
+++ src/Unicorn/ControlPanel/VisualStudio/Pipelines/UnicornControlPanelRequest/ConfigurationsVerb.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Unicorn.Configuration;
 using Unicorn.ControlPanel.Pipelines.UnicornControlPanelRequest;
 using Unicorn.ControlPanel.Responses;
