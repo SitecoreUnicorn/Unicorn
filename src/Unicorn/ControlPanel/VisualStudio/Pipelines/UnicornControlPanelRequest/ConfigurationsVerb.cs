916221b1def899d0349426925f256fdad0076ceb
Kam Figy <kamsar@gmail.com>
2016-01-26 20:27:54 -0800

    Cleanup and alterations on #104
    - Renames verb class to match the other verbs' conventions (the actual verb for the request is unchanged)
    - Adds registration of pipeline processor to Unicorn.Remote.config.disabled
    - Cleaned up usings
    - Reformat with tabs

--- src/Unicorn/ControlPanel/VisualStudio/Pipelines/UnicornControlPanelRequest/ConfigurationsVerb.cs
+++ src/Unicorn/ControlPanel/VisualStudio/Pipelines/UnicornControlPanelRequest/ConfigurationsVerb.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Unicorn.Configuration;
 using Unicorn.ControlPanel.Pipelines.UnicornControlPanelRequest;
 using Unicorn.ControlPanel.Responses;
