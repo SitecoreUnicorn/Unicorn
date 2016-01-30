718bf34fda7d2ed9189f2db5e42a620ca13bce69
Kam Figy <kamsar@gmail.com>
2016-01-29 19:06:36 -0800

    Share assembly info

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Connective DX")]
+[assembly: AssemblyProduct("Unicorn")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
+[assembly: CLSCompliant(false)]
