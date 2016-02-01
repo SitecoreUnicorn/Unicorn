df20955666c508130d871f5b0c096c21e7d3008f
Kam Figy <kamsar@gmail.com>
2016-02-01 12:40:18 -0800

    Enable role-only configurations via EmptyPredicate

---
+++ src/Unicorn.Roles/Predicates/EmptyPredicate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Rainbow.Model;
+using Rainbow.Storage;
+using Unicorn.Predicates;
+
+namespace Unicorn.Roles.Predicates
+{
+	/// <summary>
+	/// A predicate that includes no items at all. Used to allow roles-only predicates.
+	/// </summary>
+	public class EmptyPredicate : IPredicate, ITreeRootFactory
+	{
+		public string FriendlyName => "Empty Predicate";
+		public string Description => "Includes no items at all. Used for role-only configurations.";
+
+		public KeyValuePair<string, string>[] GetConfigurationDetails()
+		{
+			return new KeyValuePair<string, string>[0];
+		}
+
+		public PredicateResult Includes(IItemData itemData)
+		{
+			return new PredicateResult(false);
+		}
+
+		public TreeRoot[] GetRootPaths()
+		{
+			return new TreeRoot[0];
+		}
+
+		public IEnumerable<TreeRoot> CreateTreeRoots()
+		{
+			yield break;
+		}
+	}
+}
