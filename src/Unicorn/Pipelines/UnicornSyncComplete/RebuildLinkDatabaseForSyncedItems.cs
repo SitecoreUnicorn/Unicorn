b9913a3e65ee022eb16f21202fabe5eece7d45de
Mark Cassidy <markc@corepoint-it.net>
2016-02-29 15:58:31 +0100

    LinkDatabase auto update on Sync

---
+++ src/Unicorn/Pipelines/UnicornSyncComplete/RebuildLinkDatabaseForSyncedItems.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore;
+
+namespace Unicorn.Pipelines.UnicornSyncComplete
+{
+    public class RebuildLinkDatabaseForSyncedItems : IUnicornSyncCompleteProcessor
+    {
+        public void Process(UnicornSyncCompletePipelineArgs args)
+        {
+            foreach (var item in args.Changes)
+            {
+                if (item.ChangeType == ChangeType.Deleted)
+                    Globals.LinkDatabase.RemoveReferences(args.ProcessorItem.InnerItem);
+                else
+                    Globals.LinkDatabase.UpdateReferences(args.ProcessorItem.InnerItem);
+            }
+        }
+    }
+}
