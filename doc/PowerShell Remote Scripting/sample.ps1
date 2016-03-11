81c6ce5e42e1c682c30d426093c545f4403db1ac
Kasper Gadensgaard <kgg@nclan.netcompany.dk>
2016-03-11 18:45:10 +0100

    Improved PowerShell Remote Scripts for better support for build-server automation. Using $ScriptPath instead of . relative path. Added $url parameter to sample.ps1.

--- doc/PowerShell Remote Scripting/sample.ps1
+++ doc/PowerShell Remote Scripting/sample.ps1
@@ -1,9 +1,13 @@
+param([string]$url = 'https://localhost/unicorn.aspx')
 $ErrorActionPreference = 'Stop'
+
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 
 # This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
 
-Import-Module .\Unicorn.psm1
+Import-Module $ScriptPath\Unicorn.psm1
 
-Sync-Unicorn -ControlPanelUrl 'https://localhost/unicorn.aspx' -SharedSecret 'your-sharedsecret-here' -Configurations @('Test1', 'Test2')
+Sync-Unicorn -ControlPanelUrl $url -SharedSecret 'your-sharedsecret-here' -Configurations @('Test1', 'Test2')
 
+# Note: you may pass -Configurations @(' ') to syncronize all configurations.
 # Note: you may pass -Verb 'Reserialize' for remote reserialize. Usually not needed though.
