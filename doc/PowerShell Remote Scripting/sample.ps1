4e4f619d0fa70176765da0e8df4233217d0cf982
Kam Figy <kamsar@gmail.com>
2016-03-29 07:50:13 -0700

    Make configurations param optional for Unicorn.psm1

--- doc/PowerShell Remote Scripting/sample.ps1
+++ doc/PowerShell Remote Scripting/sample.ps1
@@ -6,7 +6,10 @@ $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 
 Import-Module $ScriptPath\Unicorn.psm1
 
+# SYNC ALL CONFIGURATIONS
+Sync-Unicorn -ControlPanelUrl 'https://localhost/unicorn.aspx' -SharedSecret 'your-sharedsecret-here'
+
+# SYNC SPECIFIC CONFIGURATIONS
 Sync-Unicorn -ControlPanelUrl 'https://localhost/unicorn.aspx' -SharedSecret 'your-sharedsecret-here' -Configurations @('Test1', 'Test2')
 
-# Note: you may pass -Configurations @(' ') to syncronize all configurations.
 # Note: you may pass -Verb 'Reserialize' for remote reserialize. Usually not needed though.
