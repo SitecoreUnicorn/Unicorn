bd721936c44137eb926a4dfde3d7f017632172d6
Kam Figy <kamsar@gmail.com>
2016-10-03 21:52:49 -0700

    3.3 RC5

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("3.3.0.0")]
 [assembly: AssemblyFileVersion("3.3.0.0")]
-[assembly: AssemblyInformationalVersion("3.3.0-RC4")]
+[assembly: AssemblyInformationalVersion("3.3.0-RC5")]
 [assembly: CLSCompliant(false)]
