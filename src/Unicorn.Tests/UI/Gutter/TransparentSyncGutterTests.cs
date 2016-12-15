7807ad138c8684c79b4887e0978d81e70f3e507c
Ben Lipson <blipson89@gmail.com>
2016-12-15 11:56:53 -0500

    Created TransparentSyncGutter

---
+++ src/Unicorn.Tests/UI/Gutter/TransparentSyncGutterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Shell.Applications.ContentEditor.Gutters;
+using Unicorn.Data.DataProvider;
+using Unicorn.Tests.TestingTools.Attributes;
+using Unicorn.UI.Gutter;
+using Xunit;
+
+namespace Unicorn.Tests.UI.Gutter
+{
+	public class TransparentSyncGutterTests
+	{
+		[Theory, AutoSub]
+		public void GetIconDescriptor_WhenItemIsNull_ThrowArgumentNullException([Greedy]TestableTransparentSyncGutter sut)
+		{
+			Assert.Throws<ArgumentNullException>(() => sut.Public_GetIconDescriptor(null));
+		}
+
+		[Theory, AutoDbData]
+		public void GetIconDescriptor_WasLastUpdatedByTransparentSync_ReturnGutterIconDescriptor([Greedy]TestableTransparentSyncGutter sut, [Content] Item item)
+		{
+			using (new EditContext(item))
+			{
+				item[FieldIDs.UpdatedBy] = UnicornDataProvider.TransparentSyncUpdatedByValue;
+				sut.Public_GetIconDescriptor(item).Should().BeAssignableTo<GutterIconDescriptor>();
+			}
+		}
+	}
+	public class TestableTransparentSyncGutter : TransparentSyncGutter
+	{
+		public GutterIconDescriptor Public_GetIconDescriptor(Item item)
+		{
+			return GetIconDescriptor(item);
+		}
+	}
+}
