6c4089bd2c138d35f70718813d2d9a8a361a3743
Kam Figy <kamsar@gmail.com>
2017-03-21 14:36:13 -0700

    4.0pre07

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-pre06")]
+[assembly: AssemblyInformationalVersion("4.0.0-pre07")]
 [assembly: CLSCompliant(false)]
