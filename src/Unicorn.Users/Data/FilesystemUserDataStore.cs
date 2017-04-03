467043f4f5dd626db1c6dc9004fe08ee85d6f27a
Wim Vergouwe <wim.vergouwe@gmail.com>
2017-04-03 08:49:11 +0200

    Support Sitecore unit testing mode

--- src/Unicorn.Users/Data/FilesystemUserDataStore.cs
+++ src/Unicorn.Users/Data/FilesystemUserDataStore.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Web.Hosting;
 using Sitecore.Diagnostics;
@@ -85,7 +86,11 @@ namespace Unicorn.Users.Data
 		{
 			if (rootPath.StartsWith("~") || rootPath.StartsWith("/"))
 			{
-				rootPath = HostingEnvironment.MapPath("~/") + rootPath.Substring(1).Replace("/", Path.DirectorySeparatorChar.ToString());
+				// Support unit testing scenario where hosting environment is not initialized.
+				var hostingRoot = HostingEnvironment.IsHosted
+					? HostingEnvironment.MapPath("~/")
+					: AppDomain.CurrentDomain.BaseDirectory;
+			    return Path.Combine(hostingRoot, rootPath.Substring(1).Replace("/", Path.DirectorySeparatorChar.ToString()));
 			}
 
 			if (!Directory.Exists(rootPath)) Directory.CreateDirectory(rootPath);
