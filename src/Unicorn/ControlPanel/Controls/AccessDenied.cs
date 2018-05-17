3a5d880f6992246167f9d9ad0bca97bcc804a13d
cassidydotdk <mark@cassidy.dk>
2018-05-17 19:58:03 +0200

    Fixes #287 - Someone left the kettle on and Sitecore 9 can't cope.

--- src/Unicorn/ControlPanel/Controls/AccessDenied.cs
+++ src/Unicorn/ControlPanel/Controls/AccessDenied.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Management.Automation.Remoting;
+using System.Web;
 using System.Web.UI;
 
 namespace Unicorn.ControlPanel.Controls
@@ -8,10 +9,27 @@ namespace Unicorn.ControlPanel.Controls
 		public void Render(HtmlTextWriter writer)
 		{
 			writer.Write("<h4>Access Denied</h4>");
+			writer.WriteLine("<!--");
+			writer.WriteLine("                       (");
+			writer.WriteLine("            _           ) )");
+			writer.WriteLine("         _,(_)._        ((");
+			writer.WriteLine("    ___,(_______).        )");
+			writer.WriteLine("  ,\'__.   /       \\    /\\_");
+			writer.WriteLine(" /,\' /  |\"\"|       \\  /  /");
+			writer.WriteLine("| | |   |__|       |,\'  /");
+			writer.WriteLine(" \\`.|                  /");
+			writer.WriteLine("  `. :           :    /");
+			writer.WriteLine("    `.            :.,\'");
+			writer.WriteLine("      `-.________,-\'");
+			writer.WriteLine("-->");
 			writer.Write("<p>You need to <a href=\"/sitecore/admin/login.aspx?ReturnUrl={0}\">sign in to Sitecore as an administrator</a> to use the Unicorn control panel.</p>", HttpUtility.UrlEncode(HttpContext.Current.Request.Url.PathAndQuery));
 
 			HttpContext.Current.Response.TrySkipIisCustomErrors = true;
-			HttpContext.Current.Response.StatusCode = 401;
+
+			// Returning 401 is causing issues on Sitecore 9. See #287 https://github.com/SitecoreUnicorn/Unicorn/issues/287
+			// Returning 418 I'm a Teapot, instead. Unicorn refuses to brew coffee to people not authenticated.
+			// Assuming it is doubtful Sitecore has, or will ever, do any special handling for this situation.
+			HttpContext.Current.Response.StatusCode = 418;
 		}
 	}
 }
