fe5149e32d2d8142dbbab994b53e3d405da8820b
Mark Cassidy <mark@cassidy.dk>
2018-07-14 23:20:55 +0200

    4.0.4-pre1

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.0.3.0")]
-[assembly: AssemblyFileVersion("4.0.3.0")]
-[assembly: AssemblyInformationalVersion("4.0.3")]
+[assembly: AssemblyVersion("4.0.4.0")]
+[assembly: AssemblyFileVersion("4.0.4.0")]
+[assembly: AssemblyInformationalVersion("4.0.4-pre1")]
 [assembly: CLSCompliant(false)]
