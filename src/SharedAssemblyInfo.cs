64443101dcd99589251a37f43cdde897ba36cf0b
Mark Cassidy <mark@cassidy.dk>
2018-08-09 00:18:24 +0200

    4.0.4-pre3

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.0.4.0")]
 [assembly: AssemblyFileVersion("4.0.4.0")]
-[assembly: AssemblyInformationalVersion("4.0.4-pre2")]
+[assembly: AssemblyInformationalVersion("4.0.4-pre3")]
 [assembly: CLSCompliant(false)]
