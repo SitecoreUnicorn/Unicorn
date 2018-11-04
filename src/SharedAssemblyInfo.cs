db83a337390c8cbc86490a5d8472027a7f8205e9
Mark Cassidy <mark@cassidy.dk>
2018-11-04 11:48:44 +0100

    Included fix for login/redirect functionality being broken on 9.0u1 and upwards

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.0.5.0")]
 [assembly: AssemblyFileVersion("4.0.5.0")]
-[assembly: AssemblyInformationalVersion("4.0.5-pre1")]
+[assembly: AssemblyInformationalVersion("4.0.5-pre2")]
 [assembly: CLSCompliant(false)]
