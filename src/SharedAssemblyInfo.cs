a3a667f63c4d9e7ed1f6fea7c5cb09df452a6ab2
Mark Cassidy <mark@cassidy.dk>
2018-12-16 15:54:10 +0100

    Amended missing setting in default Unicorn.config

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.0.5.0")]
-[assembly: AssemblyFileVersion("4.0.5.0")]
-[assembly: AssemblyInformationalVersion("4.0.5")]
+[assembly: AssemblyVersion("4.0.6.0")]
+[assembly: AssemblyFileVersion("4.0.6.0")]
+[assembly: AssemblyInformationalVersion("4.0.6")]
 [assembly: CLSCompliant(false)]
