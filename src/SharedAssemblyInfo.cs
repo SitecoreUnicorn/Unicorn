c750c4304633ba3e8f40d40a806b1ad321e9cf3b
Mark Cassidy <mark@cassidy.dk>
2019-03-24 02:14:21 +0100

    Ticked version to 4.0.8-pre2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.0.8.0")]
 [assembly: AssemblyFileVersion("4.0.8.0")]
-[assembly: AssemblyInformationalVersion("4.0.8-pre1")]
+[assembly: AssemblyInformationalVersion("4.0.8-pre2")]
 [assembly: CLSCompliant(false)]
