f03608f4ad6b986eaaadf71cf051157401c07421
Mark Cassidy <mark@cassidy.dk>
2019-09-28 23:38:37 +0200

    Minor modif of unicorn test folder

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.1.0")]
-[assembly: AssemblyFileVersion("4.1.1.0")]
-[assembly: AssemblyInformationalVersion("4.1.1")]
+[assembly: AssemblyVersion("4.1.2.0")]
+[assembly: AssemblyFileVersion("4.1.2.0")]
+[assembly: AssemblyInformationalVersion("4.1.2-pre1")]
 [assembly: CLSCompliant(false)]
