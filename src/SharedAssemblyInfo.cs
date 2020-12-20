d9c601cffc8aeae1ae1ecaf03527b88c66f42b85
Mark Cassidy <mark@cassidy.dk>
2020-12-20 15:31:52 +0100

    Ticked to 4.1.3 - minor config defaults update

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.2.0")]
-[assembly: AssemblyFileVersion("4.1.2.0")]
-[assembly: AssemblyInformationalVersion("4.1.2")]
+[assembly: AssemblyVersion("4.1.3.0")]
+[assembly: AssemblyFileVersion("4.1.3.0")]
+[assembly: AssemblyInformationalVersion("4.1.3")]
 [assembly: CLSCompliant(false)]
