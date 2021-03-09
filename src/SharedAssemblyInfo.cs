8716fe4ddb0d14165d222cebaae56dad41992805
Mark Cassidy <mark@cassidy.dk>
2021-03-09 13:25:56 +0100

    Ticked to 4.1.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Unicorn")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.3.0")]
-[assembly: AssemblyFileVersion("4.1.3.0")]
-[assembly: AssemblyInformationalVersion("4.1.3")]
+[assembly: AssemblyVersion("4.1.4.0")]
+[assembly: AssemblyFileVersion("4.1.4.0")]
+[assembly: AssemblyInformationalVersion("4.1.4")]
 [assembly: CLSCompliant(false)]
