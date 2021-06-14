81967fb13ce105d66a30caa3673c4f72cf39d9e9
Mark Cassidy <mark@cassidy.dk>
2021-06-14 17:49:51 +0200

    Fix for #374 - default config was still referring to sitecore.kernel when it should have been sitecore.buckets

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("4.1.5.0")]
 [assembly: AssemblyFileVersion("4.1.5.0")]
-[assembly: AssemblyInformationalVersion("4.1.5-pre3")]
+[assembly: AssemblyInformationalVersion("4.1.5")]
 [assembly: CLSCompliant(false)]
